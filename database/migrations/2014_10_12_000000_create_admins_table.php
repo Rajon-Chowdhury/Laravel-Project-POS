0bcdd2e275ef3a66ed8f8335bc624d98321f2d2b
Rajon Chowdhury <rajon4068@gmail.com>
2021-05-18 19:39:04 +0600

    Database table creation

--- database/migrations/2014_10_12_000000_create_admins_table.php
+++ database/migrations/2014_10_12_000000_create_admins_table.php
@@ -19,6 +19,8 @@ class CreateAdminsTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
